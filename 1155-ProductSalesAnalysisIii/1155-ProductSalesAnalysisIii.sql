cb1efcd7ec5700579e3a7f33ed1cb01ffab78a86
kiruthika <kiruthikamc110@gmail.com>
2026-07-14 14:57:04 +0530

    Create 1155-ProductSalesAnalysisIii

---
+++ 1155-ProductSalesAnalysisIii/1155-ProductSalesAnalysisIii.sql
@@ -0,0 +1,15 @@
+-- Last updated: 14/07/2026, 14:57:02
+# Write your MySQL query statement below
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) t
+ON s.product_id = t.product_id
+AND s.year = t.first_year;
