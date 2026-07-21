3b46bef95c867d9ddf18eda6c9e0b6001a721092
kiruthika <kiruthikamc110@gmail.com>
2026-07-21 09:32:13 +0530

    File updated at 21/07/2026, 09:32:00

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,36 +1,21 @@
-// Last updated: 21/07/2026, 09:30:10
-1import java.util.HashMap;
-2import java.util.Map;
-3
-4class Solution {
-5    public boolean wordPattern(String pattern, String s) {
-6        String[] words = s.split(" ");
-7        if (pattern.length() != words.length) {
-8            return false;
-9        }
-10        
-11        Map<Character, String> charToWord = new HashMap<>();
-12        Map<String, Character> wordToChar = new HashMap<>();
-13        
-14        for (int i = 0; i < pattern.length(); i++) {
-15            char c = pattern.charAt(i);
-16            String word = words[i];
-17            if (charToWord.containsKey(c)) {
-18                if (!charToWord.get(c).equals(word)) {
-19                    return false;
-20                }
-21            } else {
-22                charToWord.put(c, word);
-23            }
-24            if (wordToChar.containsKey(word)) {
-25                if (wordToChar.get(word) != c) {
-26                    return false;
-27                }
-28            } else {
-29                wordToChar.put(word, c);
-30            }
-31        }
-32        
-33        return true;
-34    }
-35}
+// Last updated: 21/07/2026, 09:32:00
+1class Solution {
+2    public boolean canConstruct(String ransomNote, String magazine) {
+3        if (ransomNote.length() > magazine.length()) {
+4            return false;
+5        }
+6
+7        int[] charCounts = new int[26];
+8        for (char c : magazine.toCharArray()) {
+9            charCounts[c - 'a']++;
+10        }
+11        for (char c : ransomNote.toCharArray()) {
+12            if (charCounts[c - 'a'] == 0) {
+13                return false; 
+14            }
+15            charCounts[c - 'a']--;
+16        }
+17
+18        return true;
+19    }
+20}
