4f2bd504b4a4e8a05c1a56ecdd270f2d05a44ded
kiruthika <kiruthikamc110@gmail.com>
2026-07-21 09:28:14 +0530

    File updated at 21/07/2026, 09:28:01

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,29 +1,16 @@
-// Last updated: 21/07/2026, 09:26:25
-1import java.util.Arrays;
-2
-3class Solution {
-4    public boolean closeStrings(String word1, String word2) {
-5        if (word1.length() != word2.length()) {
-6            return false;
-7        }
-8
-9        int[] count1 = new int[26];
-10        int[] count2 = new int[26];
-11
-12        for (char c : word1.toCharArray()) {
-13            count1[c - 'a']++;
-14        }
-15        for (char c : word2.toCharArray()) {
-16            count2[c - 'a']++;
-17        }
-18        for (int i = 0; i < 26; i++) {
-19            if ((count1[i] == 0 && count2[i] != 0) || (count1[i] != 0 && count2[i] == 0)) {
-20                return false;
-21            }
-22        }
-23        Arrays.sort(count1);
-24        Arrays.sort(count2);
-25
-26        return Arrays.equals(count1, count2);
-27    }
-28}
+// Last updated: 21/07/2026, 09:28:01
+1class Solution {
+2    public int strStr(String haystack, String needle) {
+3        int hLen = haystack.length();
+4        int nLen = needle.length();
+5        
+6        if (nLen > hLen) return -1;
+7        for (int i = 0; i <= hLen - nLen; i++) {
+8            if (haystack.substring(i, i + nLen).equals(needle)) {
+9                return i;
+10            }
+11        }
+12        
+13        return -1;
+14    }
+15}
