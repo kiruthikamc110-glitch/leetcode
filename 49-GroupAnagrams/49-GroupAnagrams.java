d0c9cfe86d6ca5487a913bffb6fe33d53703d288
kiruthika <kiruthikamc110@gmail.com>
2026-07-21 09:26:38 +0530

    File updated at 21/07/2026, 09:26:25

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,28 +1,29 @@
-// Last updated: 21/07/2026, 09:24:23
-1class Solution {
-2    public boolean validPalindrome(String s) {
-3        int left = 0;
-4        int right = s.length() - 1;
-5        
-6        while (left < right) {
-7            if (s.charAt(left) != s.charAt(right)) {
-8                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-9            }
-10            left++;
-11            right--;
-12        }
-13        
-14        return true;
-15    }
-16    
-17    private boolean isPalindrome(String s, int left, int right) {
-18        while (left < right) {
-19            if (s.charAt(left) != s.charAt(right)) {
+// Last updated: 21/07/2026, 09:26:25
+1import java.util.Arrays;
+2
+3class Solution {
+4    public boolean closeStrings(String word1, String word2) {
+5        if (word1.length() != word2.length()) {
+6            return false;
+7        }
+8
+9        int[] count1 = new int[26];
+10        int[] count2 = new int[26];
+11
+12        for (char c : word1.toCharArray()) {
+13            count1[c - 'a']++;
+14        }
+15        for (char c : word2.toCharArray()) {
+16            count2[c - 'a']++;
+17        }
+18        for (int i = 0; i < 26; i++) {
+19            if ((count1[i] == 0 && count2[i] != 0) || (count1[i] != 0 && count2[i] == 0)) {
 20                return false;
 21            }
-22            left++;
-23            right--;
-24        }
-25        return true;
-26    }
-27}
+22        }
+23        Arrays.sort(count1);
+24        Arrays.sort(count2);
+25
+26        return Arrays.equals(count1, count2);
+27    }
+28}
