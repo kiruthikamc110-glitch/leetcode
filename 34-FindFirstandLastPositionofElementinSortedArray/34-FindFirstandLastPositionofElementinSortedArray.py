17daa8495edb64ed6691e9fded65e07dcfb5ffeb
kiruthika <kiruthikamc110@gmail.com>
2026-09-24 20:47:36 +0530

    File updated at 24/09/2026, 20:47:17

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,24 +1,21 @@
-# Last updated: 24/09/2026, 20:40:45
+# Last updated: 24/09/2026, 20:47:17
 1class Solution:
-2    def isMatch(self, s, p):
-3        m = len(s)
-4        n = len(p)
-5
-6        dp = [[False] * (n + 1) for _ in range(m + 1)]
-7
-8        dp[0][0] = True
+2    def permute(self, nums):
+3        result = []
+4
+5        def backtrack(current):
+6            if len(current) == len(nums):
+7                result.append(current[:])
+8                return
 9
-10        for j in range(1, n + 1):
-11            if p[j - 1] == '*':
-12                dp[0][j] = dp[0][j - 1]
+10            for num in nums:
+11                if num not in current:
+12                    current.append(num)
 13
-14        for i in range(1, m + 1):
-15            for j in range(1, n + 1):
-16
-17                if p[j - 1] == '?' or p[j - 1] == s[i - 1]:
-18                    dp[i][j] = dp[i - 1][j - 1]
+14                    backtrack(current)
+15
+16                    current.pop()
+17
+18        backtrack([])
 19
-20                elif p[j - 1] == '*':
-21                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
-22
-23        return dp[m][n]
+20        return result
