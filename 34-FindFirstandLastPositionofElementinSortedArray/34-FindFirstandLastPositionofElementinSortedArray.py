90022e8002ce10d16df10af654db0929466c6848
kiruthika <kiruthikamc110@gmail.com>
2026-09-24 20:30:06 +0530

    File updated at 24/09/2026, 20:29:47

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,37 +1,30 @@
-# Last updated: 24/09/2026, 20:28:02
+# Last updated: 24/09/2026, 20:29:47
 1class Solution:
-2    def searchRange(self, nums, target):
-3        first = -1
-4        last = -1
-5
-6        # Find first position
-7        low = 0
-8        high = len(nums) - 1
-9
-10        while low <= high:
-11            mid = (low + high) // 2
-12
-13            if nums[mid] == target:
-14                first = mid
-15                high = mid - 1
-16            elif nums[mid] < target:
-17                low = mid + 1
-18            else:
-19                high = mid - 1
-20
-21        # Find last position
-22        low = 0
-23        high = len(nums) - 1
+2    def isValidSudoku(self, board):
+3        rows = [set() for _ in range(9)]
+4        cols = [set() for _ in range(9)]
+5        boxes = [set() for _ in range(9)]
+6
+7        for i in range(9):
+8            for j in range(9):
+9                num = board[i][j]
+10
+11                if num == ".":
+12                    continue
+13
+14                box = (i // 3) * 3 + (j // 3)
+15
+16                if num in rows[i]:
+17                    return False
+18
+19                if num in cols[j]:
+20                    return False
+21
+22                if num in boxes[box]:
+23                    return False
 24
-25        while low <= high:
-26            mid = (low + high) // 2
-27
-28            if nums[mid] == target:
-29                last = mid
-30                low = mid + 1
-31            elif nums[mid] < target:
-32                low = mid + 1
-33            else:
-34                high = mid - 1
-35
-36        return [first, last]
+25                rows[i].add(num)
+26                cols[j].add(num)
+27                boxes[box].add(num)
+28
+29        return True
