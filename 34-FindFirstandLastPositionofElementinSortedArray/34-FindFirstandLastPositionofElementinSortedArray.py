396863a0f5c2af3534150256c0c305e191b18e89
kiruthika <kiruthikamc110@gmail.com>
2026-09-24 20:56:44 +0530

    File updated at 24/09/2026, 20:56:25

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,43 +1,40 @@
-# Last updated: 24/09/2026, 20:54:10
+# Last updated: 24/09/2026, 20:56:25
 1class Solution:
-2    def solveNQueens(self, n):
-3        result = []
-4        board = [["."] * n for _ in range(n)]
-5
-6        cols = set()
-7        diag1 = set()
-8        diag2 = set()
-9
-10        def backtrack(row):
-11            if row == n:
-12                result.append(["".join(r) for r in board])
-13                return
-14
-15            for col in range(n):
-16
-17                if col in cols:
-18                    continue
-19
-20                if row - col in diag1:
-21                    continue
-22
-23                if row + col in diag2:
-24                    continue
-25
-26                # Place queen
-27                board[row][col] = "Q"
-28                cols.add(col)
-29                diag1.add(row - col)
-30                diag2.add(row + col)
+2    def totalNQueens(self, n):
+3        count = [0]
+4
+5        cols = set()
+6        diag1 = set()
+7        diag2 = set()
+8
+9        def backtrack(row):
+10            if row == n:
+11                count[0] += 1
+12                return
+13
+14            for col in range(n):
+15
+16                if col in cols:
+17                    continue
+18
+19                if row - col in diag1:
+20                    continue
+21
+22                if row + col in diag2:
+23                    continue
+24
+25                # Place queen
+26                cols.add(col)
+27                diag1.add(row - col)
+28                diag2.add(row + col)
+29
+30                backtrack(row + 1)
 31
-32                backtrack(row + 1)
-33
-34                # Remove queen
-35                board[row][col] = "."
-36                cols.remove(col)
-37                diag1.remove(row - col)
-38                diag2.remove(row + col)
-39
-40        backtrack(0)
-41
-42        return result
+32                # Remove queen
+33                cols.remove(col)
+34                diag1.remove(row - col)
+35                diag2.remove(row + col)
+36
+37        backtrack(0)
+38
+39        return count[0]
