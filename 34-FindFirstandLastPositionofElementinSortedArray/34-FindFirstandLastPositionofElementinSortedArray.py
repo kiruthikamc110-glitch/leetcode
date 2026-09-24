6b49640a4189ac8ac3c061707823ee9cd2822b7c
kiruthika <kiruthikamc110@gmail.com>
2026-09-24 20:49:35 +0530

    File updated at 24/09/2026, 20:49:16

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,31 +1,13 @@
-# Last updated: 24/09/2026, 20:48:14
+# Last updated: 24/09/2026, 20:49:16
 1class Solution:
-2    def permuteUnique(self, nums):
-3        result = []
-4        nums.sort()
-5        used = [False] * len(nums)
-6
-7        def backtrack(current):
-8            if len(current) == len(nums):
-9                result.append(current[:])
-10                return
-11
-12            for i in range(len(nums)):
-13
-14                if used[i]:
-15                    continue
-16
-17                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
-18                    continue
-19
-20                used[i] = True
-21                current.append(nums[i])
-22
-23                backtrack(current)
-24
-25                current.pop()
-26                used[i] = False
-27
-28        backtrack([])
-29
-30        return result
+2    def rotate(self, matrix):
+3        n = len(matrix)
+4
+5        # Transpose the matrix
+6        for i in range(n):
+7            for j in range(i + 1, n):
+8                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+9
+10        # Reverse each row
+11        for row in matrix:
+12            row.reverse()
