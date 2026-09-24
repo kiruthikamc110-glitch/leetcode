89a3b36060a7a0325971c6327b7d7caa40b8a7a0
kiruthika <kiruthikamc110@gmail.com>
2026-09-24 20:39:31 +0530

    File updated at 24/09/2026, 20:39:12

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,14 +1,31 @@
-# Last updated: 24/09/2026, 20:38:40
+# Last updated: 24/09/2026, 20:39:12
 1class Solution:
-2    def firstMissingPositive(self, nums):
-3        n = len(nums)
-4
-5        for i in range(n):
-6            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-7                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
-8
-9        for i in range(n):
-10            if nums[i] != i + 1:
-11                return i + 1
-12
-13        return n + 1
+2    def trap(self, height):
+3        left = 0
+4        right = len(height) - 1
+5
+6        left_max = 0
+7        right_max = 0
+8        water = 0
+9
+10        while left < right:
+11
+12            if height[left] <= height[right]:
+13
+14                if height[left] >= left_max:
+15                    left_max = height[left]
+16                else:
+17                    water += left_max - height[left]
+18
+19                left += 1
+20
+21            else:
+22
+23                if height[right] >= right_max:
+24                    right_max = height[right]
+25                else:
+26                    water += right_max - height[right]
+27
+28                right -= 1
+29
+30        return water
