185d28278577596ec43ddd6bb8141367cc986155
kiruthika <kiruthikamc110@gmail.com>
2026-09-24 20:48:33 +0530

    File updated at 24/09/2026, 20:48:14

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,21 +1,31 @@
-# Last updated: 24/09/2026, 20:47:17
+# Last updated: 24/09/2026, 20:48:14
 1class Solution:
-2    def permute(self, nums):
+2    def permuteUnique(self, nums):
 3        result = []
-4
-5        def backtrack(current):
-6            if len(current) == len(nums):
-7                result.append(current[:])
-8                return
-9
-10            for num in nums:
-11                if num not in current:
-12                    current.append(num)
+4        nums.sort()
+5        used = [False] * len(nums)
+6
+7        def backtrack(current):
+8            if len(current) == len(nums):
+9                result.append(current[:])
+10                return
+11
+12            for i in range(len(nums)):
 13
-14                    backtrack(current)
-15
-16                    current.pop()
-17
-18        backtrack([])
+14                if used[i]:
+15                    continue
+16
+17                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+18                    continue
 19
-20        return result
+20                used[i] = True
+21                current.append(nums[i])
+22
+23                backtrack(current)
+24
+25                current.pop()
+26                used[i] = False
+27
+28        backtrack([])
+29
+30        return result
